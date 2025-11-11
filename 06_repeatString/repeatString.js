f9018ed5cfa2f57ab31faf14057aa02ba75caca9
Raju Mondal <rajumondal.connect@gmail.com>
2025-11-11 22:09:09 +0530

    I just copy pested

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,10 +1,10 @@
-const repeatString = function(word,times) {
-    if(times<0)return "ERROR";
-    let string ='';
-    for(let i = 0; i < times; i++){
-        return string += word;
-    }
-    return string;
+const repeatString = function (word, times) {
+  if (times < 0) return "ERROR";
+  let string = "";
+  for (let i = 0; i < times; i++) {
+    string += word;
+  }
+  return string;
 };
 
 // Do not edit below this line
