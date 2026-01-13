462736bf142ff63ee935addc1776dfac032c3d7f
Raju Mondal <rajumondal.connect@gmail.com>
2026-01-13 19:36:59 +0530

    Complete Exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  
+	return array.reduce((a,b) => a+b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((a,b) => a*b, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num === 0){
+    return 1;
+  }
+  let result = 1;
+	for(let i=num; i>=1; i--){
+    result *= i;
+  };
+  return result;
 };
 
 // Do not edit below this line
