c9a31a56fc2903b83a0cffa66345aa2a019dff2d
Raju Mondal <rajumondal.connect@gmail.com>
2026-01-14 12:32:38 +0530

    Complete Exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let lowerString = string.toLowerCase()
+                            .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()'"?\s]/g, "");
+    let reverseString = lowerString.split('').reverse().join('');
+    if(lowerString === reverseString){
+        return true;
+    }else{
+        return false;
+    }
 };
 
 // Do not edit below this line
