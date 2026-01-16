5a64f2b000d87b0e657b0520296fddb3d0b30606
Raju Mondal <rajumondal.connect@gmail.com>
2026-01-16 15:08:40 +0530

    Complete Exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if(n<0){
+        return "OOPS"
+    }
+
+    let rootFive = Math.sqrt(5);
+    let phi = (1+rootFive)/2;
+    let psi = (1-rootFive)/2;
+    return n = Math.floor(((phi**n)-(psi**n))/rootFive);
 
 };
 
